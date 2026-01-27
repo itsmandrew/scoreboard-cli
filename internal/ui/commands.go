46c5ead054e585aef0f3942e3567eb20335b9caa
Andrew Chang <89715746+itsmandrew@users.noreply.github.com>
2026-01-26 16:51:49 -0800

    Added wrapper for sports client in Bubble Tea Command

---
+++ internal/ui/commands.go
@@ -0,0 +1,22 @@
+package ui
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/itsmandrew/scoreboard-cli/internal/sports"
+)
+
+type (
+	nbaMsg []sports.Game
+	errMsg error
+)
+
+func FetchNBACmd(apiKey string) tea.Cmd {
+	return func() tea.Msg {
+		games, err := sports.FetchNBAScores(apiKey)
+		if err != nil {
+			return errMsg(err)
+		}
+
+		return nbaMsg(games)
+	}
+}
